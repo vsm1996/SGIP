18e7470cc2132fa24bc5b364428d6ef1ee462542
Vanessa Martin <vanessa.s.martin96@gmail.com>
2025-01-12 16:16:56 -0800

    Fix firstName, lastName on register route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -20,7 +20,8 @@ export async function POST(request: NextRequest) {
   const newUser = await prisma.user.create({
     data: {
       email: body.email,
-      name: body.name,
+      firstName: body.firstName,
+      lastName: body.lastName,
       username: body.username,
       hashedPassword
     }
