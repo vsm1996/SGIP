49a209a6cc9f93c4a00b50b0d80f7c60bad35b44
Vanessa Martin <vanessa.s.martin96@gmail.com>
2025-01-16 01:09:35 -0800

    fix comment/post like icon active color

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex  min-h-dvh items-center max-lg:flex-col justify-center lg:justify-around px-16 pt-36 pb-16">
       <div className="max-lg:mb-6 prose">
         <h1>SGip 🦁</h1>
-        <h3> Welcome in! </h3>
+        <h3 className="text-balance"> The discussion space for Buddhas! </h3>
       </div>
       <div className="divider lg:divider-horizontal max-lg:mb-10" />
       <RegisterForm />
