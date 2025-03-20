3ab530a10a59b8675e726221e85ffc5534d2ae88
Vanessa Martin <vanessa.s.martin96@gmail.com>
2025-03-20 09:57:17 -0700

    hold

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
 
-    <main className="flex min-w-dvw min-h-dvh items-center justify-center prose prose-slate lg:prose-md max-lg:flex-col px-16 pt-36 pb-16">
+    <main className="flex min-w-dvw min-h-dvh items-center justify-center prose prose-slate lg:prose-md max-lg:flex-col px-16 pt-36 pb-16 bg-base-200">
       <div className="max-lg:mb-6 xl:w-1/2 text-pretty text-base-content group">
         <h1 className=" mb-8">
           Welcome to SGip <span className="inline-block group-hover:animate-wiggle">🦁 </span> <br /> &mdash; an online Buddhist practice group, dedicated to the teachings of Nichiren Daishonin.
