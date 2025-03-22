8fa8e9f42210489d2bd96aa243a77b7d1fe24e9f
Vanessa Martin <vanessa.s.martin96@gmail.com>
2025-03-22 13:16:45 -0700

    fix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-
-    <main className="flex min-w-dvw min-h-dvh items-center justify-center prose prose-slate lg:prose-md max-lg:flex-col px-16 pt-36 pb-16 bg-base-200">
-      <div className="max-lg:mb-6 xl:w-1/2 text-pretty text-base-content group">
-        <h1 className=" mb-8">
-          Welcome to SGip <span className="inline-block group-hover:animate-wiggle">🦁 </span> <br /> &mdash; an online Buddhist practice group, dedicated to the teachings of Nichiren Daishonin.
+    <main className="flex min-w-dvw min-h-dvh items-center justify-center prose prose-slate lg:prose-md max-lg:flex-col px-16 pt-36 pb-16">
+      <div className="max-lg:mb-6 xl:w-1/2 w-full text-pretty text-base-content group">
+        <h1 className=" mb-8">Welcome to SGip <span className="inline-block group-hover:animate-wiggle">🦁 </span>
+          <br /> — an online Buddhist practice group, dedicated to the teachings of Nichiren Daishonin.
         </h1>
         <h2 className="group-hover:underline mb-8">
           The purpose of this group is to provide an open and inclusive community to facilitate the propagation of Nam-Myoho-Renge-Kyo in the Latter Day of the law.
