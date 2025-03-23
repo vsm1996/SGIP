1141e760feded5c7abe23199e1c9651ec3905440
Vanessa Martin <vanessa.s.martin96@gmail.com>
2025-03-22 18:09:37 -0700

    fix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-w-dvw min-h-dvh items-center justify-center prose prose-slate lg:prose-md max-lg:flex-col px-16 pt-36 pb-16">
       <div className="max-lg:mb-6 xl:w-1/2 w-full text-pretty text-base-content group">
-        <h1 className=" mb-8">Welcome to SGip <span className="inline-block group-hover:animate-wiggle">🦁 </span>
+        <h1 className=" mb-8">Welcome to SGip <span className="inline-block group-hover:animate-wiggle"> 🦁 </span>
           <br /> — an online Buddhist practice group, dedicated to the teachings of Nichiren Daishonin.
         </h1>
         <h2 className="group-hover:underline mb-8">
