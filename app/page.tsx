f75fa48281ec831c1f89e9a31fcfc20389938574
Vanessa Martin <vanessa.s.martin96@gmail.com>
2025-06-26 18:53:51 -0700

    adding disciples of nichiren

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,16 @@ export default function Home() {
   return (
     <main className="flex min-w-dvw min-h-dvh items-center justify-center max-lg:flex-col px-8 md:px-16 py-16 bg-gradient-to-b from-base-300 to-base-100">
       <div className="max-lg:mb-10 xl:w-1/2  text-pretty text-neutral-content group backdrop-blur-sm bg-neutral/25 backdrop-opacity-10 p-8 rounded-xl shadow-lg border border-neutral animate-fade-in  hover:bg-neutral/40 transition-all duration-300">
-        <div className="flex items-center mb-6">
-          <div className="w-12 h-12 bg-primary/20 rounded-full flex items-center justify-center mr-4">
+        <div className="flex flex-col items-center mb-6">
+          <div className="w-12 bg-primary/20 rounded-full flex items-center justify-center mr-4">
             <span className="inline-block h-2/3 text-3xl group-hover:animate-wiggle">🦁</span>
           </div>
           <h1 className="text-4xl font-bold text-primary">
             Welcome to SGip
           </h1>
+          <h2>
+            - Disciples of Nichiren
+          </h2>
         </div>
 
         <div className="prose prose-slate lg:prose-lg">
